081305b1edd3964d948b5841e783efdcff8d432d
Hugh Brown <hugh@balena.io>
2020-08-05 16:45:29 -0700

    Move comment to more relevant spot
    Change-type: patch
    Signed-off-by: Hugh Brown <hugh@balena.io>

--- upgrade-supervisor/bin/index.js
+++ upgrade-supervisor/bin/index.js
@@ -3,8 +3,6 @@
 const fs = require('fs');
 const yargs = require('yargs');
 
-// Note the trailing slash here...doubled slashes *will not work*.
-// IOW, it's `${balenaUrl}v5/...`, not `${balenaUrl}/v5/...`
 const balenaUrl = "https://api.balena-staging.com/";
 const balenaToken = "/home/hugh/.balena/token.staging";
 const { getSdk } = require('balena-sdk');
@@ -36,6 +34,8 @@ balena.auth.whoami()
   });
 
 async function listSupervisorReleases(deviceType) {
+  // Doubled slashes *will not work*.  IOW, it's `${balenaUrl}v5/...`,
+  // not `${balenaUrl}/v5/...`
   reqUrl = `${balenaUrl}v5/supervisor_release?$filter=device_type%20eq%20'${deviceType}'`;
   console.log("[DEBUG] Looking for ", reqUrl);
   return await balena.request.send({url: reqUrl})
