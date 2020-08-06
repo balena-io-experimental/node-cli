d5799e074e08170ea55f08408990966abc373854
Hugh Brown <hugh@balena.io>
2020-08-06 16:23:00 -0700

    Consistify single quotes a bit
    Change-type: patch
    Signed-off-by: Hugh Brown <hugh@balena.io>

--- upgrade-supervisor/bin/index.js
+++ upgrade-supervisor/bin/index.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 const yargs = require('yargs');
 
-const balenaUrl = "https://api.balena-staging.com/";
-const balenaToken = "/home/hugh/.balena/token.staging";
+const balenaUrl = 'https://api.balena-staging.com/';
+const balenaToken = '/home/hugh/.balena/token.staging';
 const { getSdk } = require('balena-sdk');
 const balena = getSdk({
   apiUrl: balenaUrl
@@ -56,9 +56,9 @@ async function initializeBalenaAuth() {
   await balena.auth.whoami()
     .then(username => {
       if(username) {
-	console.log("[DEBUG] I am", username);
+	console.log('[DEBUG] I am', username);
       } else {
-	console.log("[DEBUG] I am nobody? I guess?")
+	console.log('[DEBUG] I am nobody? I guess?')
       }
     });
 }
@@ -68,7 +68,7 @@ async function listSupervisorReleases(deviceType, uuid) {
   // not `${balenaUrl}/v5/...`
   if (typeof deviceType === 'string') {
     reqUrl = `${balenaUrl}v5/supervisor_release?$filter=device_type%20eq%20'${deviceType}'`;
-    console.log("[DEBUG] Looking for ", reqUrl);
+    console.log('[DEBUG] Looking for ', reqUrl);
     return await balena.request.send({url: reqUrl})
       .then(data => {
 	// FIXME: implicit assumption that highest id === highest semantic version
@@ -79,7 +79,7 @@ async function listSupervisorReleases(deviceType, uuid) {
   } else if (typeof deviceType === 'string') {
     getDeviceByUUID(uuid)
       .then(device => {
-	console.log("[DEBUG] Device from API: ", device);
+	console.log('[DEBUG] Device from API:', device);
 	return device;
       })
       .then(device )
@@ -89,7 +89,7 @@ async function listSupervisorReleases(deviceType, uuid) {
 }
 
 async function setSupervisorRelease(id, deviceUUID) {
-  console.log("[DEBUG] Setting device", deviceUUID, "to be managed by supervisor ID", id);
+  console.log('[DEBUG] Setting device', deviceUUID, 'to be managed by supervisor ID', id);
   // Originally, I had the SDK call wrong and was getting this error:
 
   // (node:33536) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
@@ -100,10 +100,10 @@ async function setSupervisorRelease(id, deviceUUID) {
 
   await balena.models.device.setSupervisorRelease(deviceUUID, id)
     .then(result => {
-      console.log("Worked! ", result);
+      console.log('Worked!', result);
     })
     .catch(error => {
-      console.log("Error: ", error);
+      console.log('Error:', error);
     })
 }
 
@@ -128,14 +128,14 @@ async function upgradeSupervisor(uuid, supervisor) {
   console.log('[DEBUG] Preparing to get device type from uuid')
   getDeviceTypeFromUUID(uuid)
     .then(deviceType => {
-      console.log("[DEBUG] Device from API: ", deviceType);
+      console.log('[DEBUG] Device from API:', deviceType);
       return deviceType;
     })
     .then(deviceType => {
       return listSupervisorReleases(deviceType);
     })
     .then(release => {
-      console.log("[DEBUG] Release: ", release);
+      console.log('[DEBUG] Release:', release);
       setSupervisorRelease(release.supervisor_version, uuid);
     })
 }
